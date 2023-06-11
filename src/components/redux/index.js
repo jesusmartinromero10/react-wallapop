835e31b6cc8e639dc45114a32e52828bf00fe382
Jesus martin romero <jesusmartinromero10@gmail.com>
2023-06-11 10:46:13 +0200

    empiezo a separar reducer

--- src/components/redux/index.js
+++ src/components/redux/index.js
@@ -1,14 +1,20 @@
 //aqui creamos el store de redux
 
 import { createStore } from 'redux';
-import { reducer } from './reducer';
+import { composeWithDevTools } from '@redux-devtools/extension';
+import reducer from './reducer';
+import * as actionCreators from './actions';
+
+const composeEnhancers = composeWithDevTools({
+  //configuracion devtools
+  actionCreators,
+});
 
 export default function configureStore() {
   //para importar desde fuera y decidir desde fuera como crear el store como nos covenga mas
   const store = createStore(
     reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__(), //para que funcione las devtools de redux
+    composeEnhancers(), //para que funcione las devtools de redux
   );
   return store;
 }
--- src/components/redux/reducer.js
+++ src/components/redux/reducer.js
@@ -8,16 +8,45 @@ export const defoultState = {
 
 //creamos la funcion reducer
 
-export function reducer(state = defoultState, action) {
+// export function reducer(state = defoultState, action) {
+//   switch (action.type) {
+//     case AUTH_LOGIN:
+//       return { ...state, auth: true }; //clonamos el estado y le cambiamos la autenticacion a true o false si esta o no logeado
+//     case AUTH_LOGOUT:
+//       return { ...state, auth: false };
+//     case ADVERT_LOADED:
+//       return { ...state, adverts: action.payload }; //cogemos el payload de la accion que nos llega que seria la lista de anuncios
+
+//     default:
+//       return state;
+//   }
+// }
+
+export function auth(state = defoultState.auth, action) {
+  //esta parte del reducer solo tiene en cuenta la paerte de auth
   switch (action.type) {
     case AUTH_LOGIN:
-      return { ...state, auth: true }; //clonamos el estado y le cambiamos la autenticacion a true o false si esta o no logeado
+      return true; //clonamos el estado y le cambiamos la autenticacion a true o false si esta o no logeado
     case AUTH_LOGOUT:
-      return { ...state, auth: false };
-    case ADVERT_LOADED:
-      return { ...state, adverts: action.payload }; //cogemos el payload de la accion que nos llega que seria la lista de anuncios
+      return false;
 
     default:
       return state;
   }
+}
+
+export function adverts(state = defoultState.adverts, action) {
+  switch (action.type) {
+    case ADVERT_LOADED:
+      return action.payload; //cogemos el payload de la accion que nos llega que seria la lista de anuncios
+    default:
+      return state;
+  }
+}
+
+export default function combinedReducer(state = defoultState, action) {
+  return {
+    auth: auth(state.auth, state),
+    adverts: adverts(state.adverts, action),
+  };
 }
